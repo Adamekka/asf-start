02d329d069dc81a4ffd767f003742a9255a0004a
Adam Cvikl <adam.wasd4@gmail.com>
2022-10-05 20:25:05 +0200

    added checking if ASF-linux-x64 is placed in your home folder

--- asf.sh
+++ asf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd $HOME/ASF-linux-x64/
+cd "$HOME/ASF-linux-x64/" || { printf "Have you placed ASF-linux-x64/ folder in your home directory?\n"; exit 1; }
 xdg-open UI.html
 bash ArchiSteamFarm-Service.sh
